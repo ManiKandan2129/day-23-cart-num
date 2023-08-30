370bfdf3e8eb002bd9206ebb4507e62e12e434f4
ManiKandan2129 <gokulvirat2129@gmail.com>
2023-08-30 18:26:47 +0530

    none

--- src/AppLabel.jsx
+++ src/AppLabel.jsx
@@ -7,7 +7,7 @@ function AppLabel() {
         <div className='box-label'>
             <div>
             <h1 className='head-txt'>Shop in style</h1>
-            <h5 className='sub-txt'>With this shop homepage template</h5>
+            <h5 className='sub-txt'>With this Shop Homepage Template</h5>
             </div>
         </div>
     </div>
